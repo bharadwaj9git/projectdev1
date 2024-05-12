66d38bc8ee9aa74bd8238e7eac47dc39f6f879cc
MYPC <bharadwajsai567@gmail.com>
2024-05-12 18:13:39 +0530

    front end config file

--- Shell_script.sh
+++ Shell_script.sh
@@ -3,18 +3,11 @@
 
 dnf install nginx -y
 cp frontend.conf /etc/nginx/default.d/frontend.conf
-systemctl enable nginx
-systemctl start nginx
-
 rm -rf /usr/share/nginx/html/*
-
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
-# file required for reverse proxy
-
-#cd
-
 systemctl restart nginx
+systemctl enable nginx
+
+
